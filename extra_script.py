881a78196bdd984134cb5d19c789547612708100
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-09-06 09:30:03 +0900

    tools: use clangd

--- extra_script.py
+++ extra_script.py
@@ -1,2 +1,7 @@
 Import("env")
 env.Append(CXXFLAGS=["-Wno-register"])
+# include toolchain paths
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+
+# override compilation DB path
+# env.Replace(COMPILATIONDB_PATH=os.path.join("$BUILD_DIR", "compile_commands.json"))
