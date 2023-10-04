f7af68ab462cbafcb7823ce7bfebaaba0b4cd926
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-10-04 20:07:47 +0900

    feat: ドンファンのLSをSerial出力

--- src/mechanism.hpp
+++ src/mechanism.hpp
@@ -22,6 +22,7 @@ struct Mechanism {
   struct Donfan {
     void task() {
       bool lim[2] = {!lim_fwd->read(), !lim_rev->read()};
+      printf("don:%d ", lim[1] << 1 | lim[0]);
       if(dir == 1 && !lim[0]) {
         fp->set_raw_duty(8000);
       } else if(dir == -1 && !lim[1]) {
