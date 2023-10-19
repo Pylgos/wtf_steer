0e9a69b4a92f8c7f268979c15bb89237f800b063
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-10-19 18:29:51 +0900

    feat: おたすけのギア比を変更

--- src/mechanism.hpp
+++ src/mechanism.hpp
@@ -229,7 +229,7 @@ struct Mechanism {
     int32_t origin = 0;
   };
   struct ArmLength {
-    static constexpr int enc_interval = 2680;
+    static constexpr int enc_interval = 15000;
     static constexpr int max_length = 900;
     static constexpr float enc_to_m = 1e-3 * max_length / enc_interval;
     void task() {
