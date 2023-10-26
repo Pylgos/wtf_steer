26216afb1d9542255ea7d80e403c7b855d0f061f
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-10-26 13:00:11 +0900

    fix: オドメトリの実寸法に合わせてタイヤ径調整

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ using namespace vmath;
 
 static constexpr chrono::microseconds loop_period = 1ms;
 static constexpr float drive_motor_gear_ratio = 1.0 / 19.0;
-static constexpr float wheel_radius = 0.05;
+static constexpr float wheel_radius = 0.05 / 0.853;
 
 
 BufferedSerial pc{USBTX, USBRX, 115200};
