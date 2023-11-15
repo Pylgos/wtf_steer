ecae92d1d677be0b75b3ec4d6c2101e104b9f708
teruyamato0731 <teruyamato0731@gmail.com>
2023-11-16 08:48:32 +0900

    fix: ステアの原点合わせ

--- src/main.cpp
+++ src/main.cpp
@@ -52,10 +52,10 @@ ServoArray servo_array{140};
 Servo* const collector_servo = &servo_array[0];
 Servo* const expander_servo = &servo_array[1];
 
-Amt21 front_left_steer_enc{&rs485, 0x50, -1.0, Anglef::from_deg(77.3 - 90)};
-Amt21 rear_left_steer_enc{&rs485, 0x58, -1.0, Anglef::from_deg(19.6 + 90)};
-Amt21 rear_right_steer_enc{&rs485, 0x5C, -1.0, Anglef::from_deg(-91.5 + 270)};
-Amt21 front_right_steer_enc{&rs485, 0x54, -1.0, Anglef::from_deg(28.6 + 90)};
+Amt21 front_left_steer_enc{&rs485, 0x50, -1.0, Anglef::from_deg(-14.2)};
+Amt21 rear_left_steer_enc{&rs485, 0x58, -1.0, Anglef::from_deg(-13.6 - 11.7 + 16.4)};
+Amt21 rear_right_steer_enc{&rs485, 0x5C, -1.0, Anglef::from_deg(-86.4)};
+Amt21 front_right_steer_enc{&rs485, 0x54, -1.0, Anglef::from_deg(119.3)};
 std::array<Amt21*, 4> steer_encoders = {
     &front_left_steer_enc, &rear_left_steer_enc, &rear_right_steer_enc, &front_right_steer_enc};
 
